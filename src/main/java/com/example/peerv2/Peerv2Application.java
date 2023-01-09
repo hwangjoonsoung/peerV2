36b9fd8a97a0d83a8096bfa5667ff77faaa9d0d3
hwangjoonsoung <joonsoung93@naver.com>
2023-01-09 17:43:24 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/peerv2/Peerv2Application.java
@@ -0,0 +1,13 @@
+package com.example.peerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Peerv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Peerv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/peerv2/Peerv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Peerv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
